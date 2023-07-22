a1142f01d9036d4f5f3bf4489fb73937d5e74016
belafdil-chakib <84620047+belafdil-chakib@users.noreply.github.com>
2023-07-22 12:44:41 +0200

    Update test_mean.py
    Add unit tests

--- tests/test_mean.py
+++ tests/test_mean.py
@@ -8,8 +8,16 @@ class TestSuite(unittest.TestCase):
     def setUpClass(cls):
         super(TestSuite, cls).setUpClass()
 
-        cls.input_numbers = list(range(1, 6))
-        cls.expected_mean = 3
+        cls.input_numbers_1 = list(range(1, 6))
+        cls.expected_mean_1 = 3
 
+    cls.input_numbers_2 = list(range(1, 5))
+    cls.expected_mean_2 = 2.5
+
+    cls.input_numbers_3 = list(1)
+    cls.expected_mean_3 = 1
+    
     def test_mean(self):
-        self.assertEqual(self.expected_mean, mean(self.input_numbers))
+        self.assertEqual(self.expected_mean_1, mean(self.input_numbers_1))
+        self.assertEqual(self.expected_mean_2, mean(self.input_numbers_2))
+        self.assertEqual(self.expected_mean_3, mean(self.input_numbers_3))
